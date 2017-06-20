ed2c554bf34bd56e46e6fe0f1a6c1264f3d13526
中村勇哉 <nakamurayuuya@nakamurayuuya-no-MacBook-Air.local>
2017-06-20 14:55:21 +0900

    一覧投稿保存

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,20 @@
+class PicturesController < ApplicationController
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    Picture.create(pictures_params)
+    redirect_to pictures_path
+  end
+
+  private
+   def pictures_params
+     params.require(:picture).permit(:title,:content)
+   end
+end
